c84005179cda30a8b8cf3d80f672a9c292a15837
BhavishTrehan77 <bhavishtrehan777@gmail.com>
2026-05-17 08:44:41 +0530

    made a mini counter application for practicing useState

---
+++ @latest/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const handleIncrement=()=>{
+    setCount(count+1)
+  }
+  const handleDecrement=()=>{
+    if(count>0){
+      setCount(count-1)
+    }
+  }
+
+
+  return (
+    <>
+    <h1>COUNTER-APP</h1>
+      <button onClick={handleIncrement}>INCREMENT</button>
+      <button onClick={handleDecrement}>DECREMENT</button>
+      <div>
+        <h2>THE VALUE OF COUNT IS {count}</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
